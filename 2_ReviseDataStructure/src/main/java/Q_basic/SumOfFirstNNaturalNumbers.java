99f3eaaf7672762c360a611a65b1190f14849275
Kushal <52573092+KushalK2507@users.noreply.github.com>
2025-04-15 13:30:50 +0530

    Refactor 2_ReviseDataStructure

---
+++ 2_ReviseDataStructure/src/main/java/Q_basic/SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,14 @@
+package Q_basic;
+
+public class SumOfFirstNNaturalNumbers {
+
+
+    public static void main (String[] args){
+
+        int n=4;
+        // Finding Sum of N Natural Numbers with O(1) complexity
+        int sum = n*(n+1)/2;
+        System.out.println("Sum of First N Natural Numbers "+sum);
+
+    }
+}
---
+++ 2_ReviseDataStructure/src/main/java/Q_basic/SwapWithoutUsingThirdVariable.java
@@ -0,0 +1,17 @@
+package Q_basic;
+
+public class SwapWithoutUsingThirdVariable {
+
+    public static void main (String[] args){
+
+        int a=10;
+        int b=15;
+        System.out.println("Orignal Number: "+a+" "+b);
+         a= a+b;
+         b=a-b;
+         a = a-b;
+         System.out.println("Swapped Number: "+a+" "+b);
+
+
+    }
+}
